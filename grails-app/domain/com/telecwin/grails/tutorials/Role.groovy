bb75d6be17ef79da8694e2dd072572e0d00839be
bo.yang <bo.yang@telecwin.com>
2020-02-27 20:49:58 +0800

    clean code

--- grails-app/domain/com/telecwin/grails/tutorials/Role.groovy
+++ grails-app/domain/com/telecwin/grails/tutorials/Role.groovy
@@ -1,23 +1,23 @@
 package com.telecwin.grails.tutorials
 
+import grails.compiler.GrailsCompileStatic
 import groovy.transform.EqualsAndHashCode
 import groovy.transform.ToString
-import grails.compiler.GrailsCompileStatic
 
+@SuppressWarnings("unused")
 @GrailsCompileStatic
-@EqualsAndHashCode(includes='authority')
-@ToString(includes='authority', includeNames=true, includePackage=false)
+@EqualsAndHashCode(includes = 'authority')
+@ToString(includes = 'authority', includeNames = true, includePackage = false)
 class Role implements Serializable {
 
-	private static final long serialVersionUID = 1
+    private static final long serialVersionUID = 1
 
-	String authority
+    String authority
 
-	static constraints = {
-		authority nullable: false, blank: false, unique: true
-	}
+    static constraints = {
+        authority nullable: false, blank: false, unique: true
+    }
 
-	static mapping = {
-		cache true
-	}
+    static mapping = {
+    }
 }
