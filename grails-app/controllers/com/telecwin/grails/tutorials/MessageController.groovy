df3095c6a2dd0c3debbc950730d3f0d97df98cef
bo.yang <bo.yang@telecwin.com>
2020-02-28 21:02:18 +0800

    添加一些日志

--- grails-app/controllers/com/telecwin/grails/tutorials/MessageController.groovy
+++ grails-app/controllers/com/telecwin/grails/tutorials/MessageController.groovy
@@ -1,13 +1,13 @@
 package com.telecwin.grails.tutorials
 
-
 import grails.plugin.springsecurity.annotation.Secured
 import groovy.json.JsonOutput
 import groovy.json.JsonSlurper
+import groovy.util.logging.Slf4j
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.handler.annotation.SendTo
 
-
+@Slf4j
 class MessageController {
 
     @Secured("permitAll")
@@ -18,6 +18,7 @@ class MessageController {
     // 这是 controller 发送消息的目的地
     @SendTo("/topic/hello")
     protected static def hello(String world) {
+        log.info("收到消息：$world")
         JsonSlurper jsonSlurper = new JsonSlurper()
         def msg = jsonSlurper.parseText(world)
         return JsonOutput.toJson("hello, ${msg}")
