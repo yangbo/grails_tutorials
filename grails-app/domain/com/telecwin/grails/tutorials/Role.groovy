423c10c4ee3a9bcc7c31e80cc296267356107a42
bo.yang <bo.yang@telecwin.com>
2020-04-15 13:12:58 +0800

    去掉IDEA编译错误

--- grails-app/domain/com/telecwin/grails/tutorials/Role.groovy
+++ grails-app/domain/com/telecwin/grails/tutorials/Role.groovy
@@ -1,23 +1,28 @@
 package com.telecwin.grails.tutorials
 
+import grails.compiler.GrailsCompileStatic
 import groovy.transform.EqualsAndHashCode
 import groovy.transform.ToString
-import grails.compiler.GrailsCompileStatic
+
+import static grails.gorm.hibernate.mapping.MappingBuilder.orm
 
 @GrailsCompileStatic
-@EqualsAndHashCode(includes='authority')
-@ToString(includes='authority', includeNames=true, includePackage=false)
+@EqualsAndHashCode(includes = 'authority')
+@ToString(includes = 'authority', includeNames = true, includePackage = false)
+@SuppressWarnings("unused")
 class Role implements Serializable {
 
-	private static final long serialVersionUID = 1
+    private static final long serialVersionUID = 1
 
-	String authority
+    String authority
 
-	static constraints = {
-		authority nullable: false, blank: false, unique: true
-	}
+    static constraints = {
+        authority nullable: false, blank: false, unique: true
+    }
 
-	static mapping = {
-		cache true
-	}
+    static final mapping = orm {
+        cache {
+            enabled true
+        }
+    }
 }
--- grails-app/domain/com/telecwin/grails/tutorials/UserRole.groovy
+++ grails-app/domain/com/telecwin/grails/tutorials/UserRole.groovy
@@ -1,87 +1,93 @@
 package com.telecwin.grails.tutorials
 
+import grails.compiler.GrailsCompileStatic
 import grails.gorm.DetachedCriteria
 import groovy.transform.ToString
-
 import org.codehaus.groovy.util.HashCodeHelper
-import grails.compiler.GrailsCompileStatic
+
+import static grails.gorm.hibernate.mapping.MappingBuilder.orm
 
 @GrailsCompileStatic
-@ToString(cache=true, includeNames=true, includePackage=false)
+@ToString(cache = true, includeNames = true, includePackage = false)
+@SuppressWarnings("unused")
 class UserRole implements Serializable {
 
-	private static final long serialVersionUID = 1
+    private static final long serialVersionUID = 1
 
-	User user
-	Role role
-
-	@Override
-	boolean equals(other) {
-		if (other instanceof UserRole) {
-			other.userId == user?.id && other.roleId == role?.id
-		}
-	}
+    User user
+    Role role
 
     @Override
-	int hashCode() {
-	    int hashCode = HashCodeHelper.initHash()
+    boolean equals(other) {
+        if (other instanceof UserRole) {
+            other.userId == user?.id && other.roleId == role?.id
+        } else {
+            false
+        }
+    }
+
+    @Override
+    int hashCode() {
+        int hashCode = HashCodeHelper.initHash()
         if (user) {
             hashCode = HashCodeHelper.updateHash(hashCode, user.id)
-		}
-		if (role) {
-		    hashCode = HashCodeHelper.updateHash(hashCode, role.id)
-		}
-		hashCode
-	}
+        }
+        if (role) {
+            hashCode = HashCodeHelper.updateHash(hashCode, role.id)
+        }
+        hashCode
+    }
 
-	static UserRole get(long userId, long roleId) {
-		criteriaFor(userId, roleId).get()
-	}
+    static UserRole get(long userId, long roleId) {
+        criteriaFor(userId, roleId).get()
+    }
 
-	static boolean exists(long userId, long roleId) {
-		criteriaFor(userId, roleId).count()
-	}
+    static boolean exists(long userId, long roleId) {
+        criteriaFor(userId, roleId).count()
+    }
 
-	private static DetachedCriteria criteriaFor(long userId, long roleId) {
-		UserRole.where {
-			user == User.load(userId) &&
-			role == Role.load(roleId)
-		}
-	}
+    private static DetachedCriteria criteriaFor(long userId, long roleId) {
+        where {
+            user == User.load(userId) &&
+                    role == Role.load(roleId)
+        }
+    }
 
-	static UserRole create(User user, Role role, boolean flush = false) {
-		def instance = new UserRole(user: user, role: role)
-		instance.save(flush: flush)
-		instance
-	}
+    static UserRole create(User user, Role role, boolean flush = false) {
+        def instance = new UserRole(user: user, role: role)
+        instance.save(flush: flush)
+        instance
+    }
 
-	static boolean remove(User u, Role r) {
-		if (u != null && r != null) {
-			UserRole.where { user == u && role == r }.deleteAll()
-		}
-	}
+    static boolean remove(User u, Role r) {
+        if (u != null && r != null) {
+            where { user == u && role == r }.deleteAll()
+        } else {
+            false
+        }
+    }
 
-	static int removeAll(User u) {
-		u == null ? 0 : UserRole.where { user == u }.deleteAll() as int
-	}
+    static int removeAll(User u) {
+        u == null ? 0 : where { user == u }.deleteAll() as int
+    }
 
-	static int removeAll(Role r) {
-		r == null ? 0 : UserRole.where { role == r }.deleteAll() as int
-	}
+    static int removeAll(Role r) {
+        r == null ? 0 : where { role == r }.deleteAll() as int
+    }
 
-	static constraints = {
-	    user nullable: false
-		role nullable: false, validator: { Role r, UserRole ur ->
-			if (ur.user?.id) {
-				if (UserRole.exists(ur.user.id, r.id)) {
-				    return ['userRole.exists']
-				}
-			}
-		}
-	}
+    static constraints = {
+        user nullable: false
+        role nullable: false, validator: { Role r, UserRole ur ->
+            if (ur.user?.id) {
+                if (exists(ur.user.id, r.id)) {
+                    return ['userRole.exists']
+                }
+            }
+        }
+    }
 
-	static mapping = {
-		id composite: ['user', 'role']
-		version false
-	}
+    static final mapping = orm {
+        id composite: ['user', 'role']
+        version false
+    }
 }
